ffd4393d76cbf8f93721a543e44860826ab1ce9b
Andrew Hall (METAL) <andrha@microsoft.com>
2024-07-02 15:58:09 -0700

    Add benchmark and make stream extensions for netcore only

---
+++ src/Razor/benchmarks/Microsoft.AspNetCore.Razor.Microbenchmarks/Serialization/RazorProjectInfoSerializationBenchmark_NetOnly.cs
@@ -0,0 +1,137 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+#if NET
+using System;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.IO.Pipelines;
+using System.Threading;
+using Microsoft.AspNetCore.Razor.Language;
+using Microsoft.AspNetCore.Razor.ProjectSystem;
+using Microsoft.AspNetCore.Razor.Serialization;
+using Microsoft.AspNetCore.Razor.Utilities;
+using Microsoft.CodeAnalysis.Razor.ProjectSystem;
+#endif
+
+namespace Microsoft.AspNetCore.Razor.Microbenchmarks.Serialization;
+
+public class RazorProjectInfoSerializationBenchmark_NetOnly
+{
+    // These benchmarks are only intended to run in NET5+ as the serialization/deserialization logic already has that requirement
+#if NET
+
+    [ParamsAllValues]
+    public ResourceSet ResourceSet { get; set; }
+
+    [Params(100, 1000, 10000000)]
+    public int FileCount { get; set; }
+
+    [Params(1, 10)]
+    public int SerializationCount { get; set; }
+
+    private ImmutableArray<TagHelperDescriptor> TagHelpers
+        => ResourceSet switch
+        {
+            ResourceSet.Telerik => CommonResources.TelerikTagHelpers,
+            _ => CommonResources.LegacyTagHelpers
+        };
+
+    [AllowNull]
+    private RazorProjectInfo _projectInfo;
+
+    [AllowNull]
+    private Pipe _pipe;
+
+    [GlobalSetup(Targets = [nameof(RoundTrip_Serialization)])]
+    public void GlobalSetup()
+    {
+        _pipe = new();
+        _projectInfo = new RazorProjectInfo(
+            new ProjectKey(@"C:\project\output\obj"),
+            @"C:\project\project.csproj",
+            FallbackRazorConfiguration.Latest,
+            "TestNamespace",
+            "Project",
+            ProjectWorkspaceState.Create(TagHelpers, CodeAnalysis.CSharp.LanguageVersion.Latest),
+            GetDocuments(@"C:\project", FileCount));
+    }
+
+    private ImmutableArray<DocumentSnapshotHandle> GetDocuments(string baseDurectory, int fileCount)
+    {
+        var documentShapshots = new DocumentSnapshotHandle[fileCount];
+
+        for (var i = 0; i < fileCount; i++)
+        {
+            var documentFullPath = Path.Combine(baseDurectory, "Pages", $"Page{i}.razor");
+            var outputPath = Path.Combine(baseDurectory, "out", $"Page{i}.razor");
+            documentShapshots[i] = new DocumentSnapshotHandle(documentFullPath, outputPath, FileKinds.Component);
+        }
+
+        return documentShapshots.ToImmutableArray();
+    }
+
+    [Benchmark(Description = "Round Trip Serialization")]
+    public async Task RoundTrip_Serialization()
+    {
+        var reader = _pipe.Reader;
+        var writer = _pipe.Writer;
+
+        Debug.WriteLine($"Starting test with iterations: {SerializationCount}");
+
+        var writerTask = Task.Run(async () =>
+        {
+            try
+            {
+                for (var i = 0; i < SerializationCount; i++)
+                {
+                    Debug.WriteLine($"Writing iteration {i}");
+                    await writer.AsStream(leaveOpen: true).WriteProjectInfoAsync(_projectInfo, CancellationToken.None);
+                    await writer.FlushAsync();
+                }
+
+                await writer.CompleteAsync();
+            }
+            catch (InvalidOperationException)
+            {
+                // just in case we try to read too much, we can ignore this.
+            }
+        });
+
+        var readerTask = Task.Run(async () =>
+        {
+            try
+            {
+                for (var i = 0; i < SerializationCount; i++)
+                {
+                    using var readStream = reader.AsStream(leaveOpen: true);
+                    Debug.WriteLine($"Reading iteration {i}");
+                    var action = readStream.ReadProjectInfoAction();
+
+                    Debug.WriteLine($"Deserializing project {i}");
+                    _ = await readStream.ReadProjectInfoAsync(CancellationToken.None);
+                }
+
+                await reader.CompleteAsync();
+            }
+            catch (InvalidOperationException)
+            {
+                // just in case we try to read too much, we can ignore this.
+            }
+        });
+
+        await Task.WhenAll(writerTask, readerTask);
+
+        Debug.WriteLine("Completed test");
+    }
+#else
+    [Benchmark(Description = "Round Trip Serialization")]
+    public Task RoundTrip_Serialization() => Task.CompletedTask;
+    #endif
+
+}
--- src/Razor/src/Microsoft.AspNetCore.Razor.ProjectEngineHost/Utilities/NetStreamExtensions.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.ProjectEngineHost/Utilities/NetStreamExtensions.cs
@@ -1,6 +1,9 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
+// These extensions are only used in NET5+ and are using APIs not available in NetFX
+#if NET
+
 using System;
 using System.Buffers;
 using System.Diagnostics;
@@ -10,14 +13,9 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Razor.ProjectSystem;
 
-#if !NET
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-#endif
-
 namespace Microsoft.AspNetCore.Razor.Utilities;
 
-internal static class StreamExtensions
+internal static class NetStreamExtensions
 {
     public static Task WriteStringAsync(this Stream stream, string text, Encoding? encoding, CancellationToken cancellationToken)
     {
@@ -27,25 +25,10 @@ internal static class StreamExtensions
         var byteCount = encoding.GetMaxByteCount(text.Length);
         using var _ = ArrayPool<byte>.Shared.GetPooledArray(byteCount, out var byteArray);
 
-#if NET
         var usedBytes = encoding.GetBytes(text, byteArray);
-#else
-        var usedBytes = GetBytes(text, encoding, byteArray);
-#endif
 
         WriteSize(stream, usedBytes);
         return stream.WriteAsync(byteArray, 0, usedBytes, cancellationToken);
-
-#if !NET
-        static unsafe int GetBytes(string text, Encoding encoding, byte[] byteArray)
-        {
-            fixed (char* c = text)
-            fixed (byte* b = byteArray)
-            {
-                return encoding.GetBytes(c, text.Length, b, byteArray.Length);
-            }
-        }
-#endif
     }
 
     public static async Task<string> ReadStringAsync(this Stream stream, Encoding? encoding, CancellationToken cancellationToken)
@@ -112,29 +95,16 @@ internal static class StreamExtensions
 
     private static void WriteSize(Stream stream, int length)
     {
-#if NET
         Span<byte> sizeBytes = stackalloc byte[4];
         BitConverter.TryWriteBytes(sizeBytes, length);
         stream.Write(sizeBytes);
-#else
-        using var _ = ArrayPool<byte>.Shared.GetPooledArray(4, out var sizeBytes);
-        // Pulled from https://github.com/dotnet/runtime/blob/4b9a1b2d956f4a10a28b8f5f3f725e76eb6fb826/src/libraries/System.Private.CoreLib/src/System/BitConverter.cs#L158C13-L158C87
-        Unsafe.WriteUnaligned(ref MemoryMarshal.GetReference(sizeBytes.AsSpan()), length);
-        stream.Write(sizeBytes, 0, 4);
-#endif
-
     }
 
     private unsafe static int ReadSize(Stream stream)
     {
-#if NET
         Span<byte> bytes = stackalloc byte[4];
         stream.Read(bytes);
         return BitConverter.ToInt32(bytes);
-#else
-        using var _  = ArrayPool<byte>.Shared.GetPooledArray(4, out var bytes);
-        stream.Read(bytes, 0, 4);
-        return BitConverter.ToInt32(bytes, 0);
-#endif
     }
 }
+#endif
